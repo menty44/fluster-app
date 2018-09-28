efc1ec0303491a08d12b5061dc7968088f6c8004
Quang Anh <quanganh@aiti.com.vn>
2018-09-28 09:57:21 +0700

    Added: Docs and Diagram

---
+++ diagram.sh
@@ -0,0 +1,47 @@
+tsviz -r -d ./src/app/services/model/ ./diagram/service_model.png
+tsviz -r ./src/app/services/model/appointment/applicant.ts ./diagram/service_model_appointment.png
+tsviz -r ./src/app/services/model/chat/ ./diagram/service_model_chat.png
+tsviz -r ./src/app/services/model/device/ ./diagram/service_model_device.png
+tsviz -r ./src/app/services/model/item/ ./diagram/service_model_item.png
+tsviz -r ./src/app/services/model/location/ ./diagram/service_model_location.png
+tsviz -r ./src/app/services/model/notification/ ./diagram/service_model_notification.png
+tsviz -r ./src/app/services/model/product/ ./diagram/service_model_product.png
+tsviz -r ./src/app/services/model/reward/ ./diagram/service_model_reward.png
+tsviz -r ./src/app/services/model/user/ ./diagram/service_model_user.png
+tsviz -r ./src/app/services/model/utils/ ./diagram/service_model_utils.png
+
+tsviz -r -d ./src/app/services/core/ ./diagram/service_core.png
+tsviz -r ./src/app/services/core/amazon/ ./diagram/service_core_amazon.png
+tsviz -r ./src/app/services/core/appointment/ ./diagram/service_core_appointment.png
+tsviz -r ./src/app/services/core/authentication/ ./diagram/service_core_authentication.png
+tsviz -r ./src/app/services/core/chat/ ./diagram/service_core_chat.png
+tsviz -r ./src/app/services/core/complaint/ ./diagram/service_core_complaint.png
+tsviz -r ./src/app/services/core/currency/ ./diagram/service_core_currency.png
+tsviz -r ./src/app/services/core/deeplinking/ ./diagram/service_core_deeplinking.png
+tsviz -r ./src/app/services/core/error/ ./diagram/service_core_error.png
+tsviz -r ./src/app/services/core/gesture/ ./diagram/service_core_gesture.png
+tsviz -r ./src/app/services/core/http/ ./diagram/service_core_http.png
+tsviz -r ./src/app/services/core/languages/ ./diagram/service_core_languages.png
+tsviz -r ./src/app/services/core/localstorage/ ./diagram/service_core_localstorage.png
+tsviz -r ./src/app/services/core/location/ ./diagram/service_core_location.png
+tsviz -r ./src/app/services/core/login/ ./diagram/service_core_login.png
+tsviz -r ./src/app/services/core/map/ ./diagram/service_core_map.png
+tsviz -r ./src/app/services/core/navigation/ ./diagram/service_core_navigation.png
+tsviz -r ./src/app/services/core/notification/ ./diagram/service_core_notification.png
+tsviz -r ./src/app/services/core/permission/ ./diagram/service_core_permission.png
+tsviz -r ./src/app/services/core/product/ ./diagram/service_core_product.png
+tsviz -r ./src/app/services/core/reward/ ./diagram/service_core_reward.png
+tsviz -r ./src/app/services/core/routing/ ./diagram/service_core_routing.png
+tsviz -r ./src/app/services/core/spotify/ ./diagram/service_core_spotify.png
+tsviz -r ./src/app/services/core/user/ ./diagram/service_core_user.png
+tsviz -r ./src/app/services/core/utils/ ./diagram/service_core_utils.png
+tsviz -r ./src/app/services/core/work-school/ ./diagram/service_core_work_school.png
+tsviz -r ./src/app/services/core/yelp/ ./diagram/service_core_yelp.png
+
+tsviz -r ./src/app/services/native/analytics/ ./diagram/service_native_analytics.png
+tsviz -r ./src/app/services/native/calendar/ ./diagram/service_native_calendar.png
+tsviz -r ./src/app/services/native/facebook/ ./diagram/service_native_facebook.png
+tsviz -r ./src/app/services/native/google/ ./diagram/service_native_google.png
+tsviz -r ./src/app/services/native/localfiles/ ./diagram/service_native_localfiles.png
+
+tsviz -r ./src/app/services/types/ ./diagram/service_types.png
